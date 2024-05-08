0a4a3b85181af7da9df317e260c61b56373e48f0
Dalton Grimes <159388530+grimesdl@users.noreply.github.com>
2024-05-08 17:36:31 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -15,16 +15,11 @@ def generate_test_data(num_contacts):
     db.execute('''ALTER TABLE contacts ADD COLUMN favorite TEXT''')
     db.commit()
 
-    # Add "favorite moment" column to table if it doesn't already exist
-    db.execute('''ALTER TABLE contacts ADD COLUMN moment TEXT''')
-    db.commit()
-    
     for i in range(num_contacts):
         name = f'Test Name {i}'
         phone = f'123-456-789{i}'
         favorite = f'My favorite class was CIT225{i}' # Add favorite class
-        moment = f'My favorite moment was graduating{i}' # Add favorite moment
-        db.execute('INSERT INTO contacts (name, phone, favorite, moment) VALUES (?, ?, ?, ?)', (name, phone, favorite, moment)) # Include favorite class and moment in INSERT statement
+        db.execute('INSERT INTO contacts (name, phone, favorite) VALUES (?, ?, ?)', (name, phone, favorite)) # Include favorite class in INSERT statement
     db.commit()
     print(f'{num_contacts} test contacts added to the database.')
     db.close()
